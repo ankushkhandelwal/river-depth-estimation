3e6b5bad4ce42044371a2f2f099a9196f03b725a
ankushkhandelwal <ankush.kwal@gmail.com>
2019-08-23 10:22:16 -0500

    fixing folder create error in the test script

--- scripts/wings_test.py
+++ scripts/wings_test.py
@@ -11,10 +11,11 @@ base_dir = sys.argv[1]
 prefix = sys.argv[2]
 numsamples = int(sys.argv[3])
 folder_name = base_dir + 'Boxes/'
+if os.path.isdir(folder_name)==True:
+	os.system('rm -rf ' + folder_name)
 os.mkdir(folder_name)
 for i in range(numsamples):
 	fid = open(folder_name + prefix + '-' + str(i) + '.txt','w')
 	fid.write(str(i)+ ',' + str(i+1) + '\n')
 	fid.close()
 sys.exit()
-
